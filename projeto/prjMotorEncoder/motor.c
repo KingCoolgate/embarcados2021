2a6c9055d031d0ab33495ea1abc9aef86b5886b7
Colgate_MK_016 <joaopedroishida@gmail.com>
2021-06-09 16:13:20 -0300

    conversas iniciais sobre o projeto

---
+++ projeto/prjMotorEncoder/motor.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <pthreads.h>
+#include <math.h>
+
+
+.
+.
+.
+.
+.
+.
+
+
+int saidaProMotor(posicaoAtual, portaComunicacao, portaSaida, ....){
+	s = posicao;
+	ptc = porta;
+	pts = portaS;
+	
+	.
+	.
+	.
+	
+	return(dpp)
+}
+
+int thread1(){
+	while(true){
+	tensao = saidaProMotor(x, y, z, w);
+	}
+
+}
+
+int envio(dauibfailsdf){
+	pegar o valor de tensao
+	manda para a porta X
+}
+
+int thread2(){
+	while(true){
+		envio(tesao, porta);
+	}
+}
+
+int main (int argc, char *argv[]) {
+	cria a thread (ponteiro, nome, função, arguementos da função);
+	cria a thread (x, y, z, w)
+}
