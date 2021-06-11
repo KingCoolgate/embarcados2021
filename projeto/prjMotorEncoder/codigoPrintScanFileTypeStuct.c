3cc1dcc558538378b859270610d61f2a5d71c20d
Colgate_MK_016 <joaopedroishida@gmail.com>
2021-06-11 15:51:35 -0300

    fazendo as coisas pelo atom, parece muito mais fácil

--- projeto/prjMotorEncoder/codigoPrintScanFileTypeStuct.c
+++ projeto/prjMotorEncoder/codigoPrintScanFileTypeStuct.c
@@ -13,14 +13,14 @@ typedef struct
 	float N1, N2, media;
 } T_aluno;
 
-char chk[] = "sim", nome[] = " Nome : ", nro[] = ".\n Nro USP eh : ", N1[] = ".\n Nota da P1 e : ", N2[] = ".\n Nota da P2 eh : ", media[] = ".\n Media ele eh : ", pula[] = "\n" ;
+char chk[] = "sim", nome[] = " Nome : ", nro[] = ".\n Nro USP : ", N1[] = ".\n Nota da P1 : ", N2[] = ".\n Nota da P2 : ", media[] = ".\n Media dele : ", pula[] = "\n" ;
 
-int flag = 0, a = 0, r = 0, i;
+int flag = 0, a = 0, r = 0, i, b=0;
 int turma, peso1, peso2, medaprov;
 
 void infoturma()
-{	
-	printf ("\n -----DIGITE AS INFORMACOES DA TURMA----- \n");		
+{
+	printf ("\n -----DIGITE AS INFORMACOES DA TURMA----- \n");
 	printf("\n Digite o tamanho da Turma : ");
 		scanf ("%d", &turma);
 	printf(" Digite a media para aprovacao : ");
@@ -28,7 +28,7 @@ void infoturma()
 	printf(" Digite o peso da prova1 : ");
 		scanf ("%d", &peso1);
 	printf(" Digite o peso da prova2 : ");
-		scanf ("%d", &peso2);	
+		scanf ("%d", &peso2);
 
 	while ( (peso1+peso2) <= 0 )
 	{
@@ -36,36 +36,46 @@ void infoturma()
 		printf(" Digite o peso da prova1 novamente : ");
 			scanf ("%d", &peso1);
 		printf(" Digite o peso da prova2 novamente : ");
-			scanf ("%d", &peso2);
+			scanf ("%d ^[\n]", &peso2);
 	}
-	printf ("\n***************************************************************************************************\n");			
+	printf ("\n***************************************************************************************************\n");
+}
+
+void nomealuno(T_aluno aluno[turma])
+{
+	//Pega as informações do aluno
+		printf ("\n -----DIGITE AS INFORMACOES DO ALUNO----- \n");
+			//Nome
+				fflush(stdin);
+					printf ("\n Digite o nome do aluno: ");
+					while(b == 0)
+					{
+						gets ( aluno[flag].nome);
+						fflush(stdin);
+
+						if (strlen(aluno[flag].nome) != 0)
+						{
+							b = 1;
+						}
+					}
 }
 
 void infoaluno(T_aluno aluno[turma])
 {
-//Pega as informações do aluno
-	printf ("\n -----DIGITE AS INFORMACOES DO ALUNO----- \n");		
-		//Nome
-			fflush(stdin);
-				printf ("\n Digite o nome do aluno: ");
-					 fgets ( aluno[flag].nome, MAX_FLOW_SIZE, stdin);
-					 sleep (2);
-			fflush(stdin);
-					
-		//NroUSP	
+		//NroUSP
 			printf (" Digite o numero USP do aluno : ");
 				scanf ("%d", &aluno[flag].nro);
-			
+
 		//Nota da P1
 			printf (" De o valor da P1 do aluno: ");
-       			scanf  ("%f", &aluno[flag].N1);	
+       			scanf  ("%f", &aluno[flag].N1);
 				   while (( aluno[flag].N1 > 10 ) || ( aluno[flag].N1 < 0 ))
        			 	{
        			 		printf (" Nota invalida! Insira novamente : ");
 						scanf ("%f", &aluno[flag].N1);
 					}
-					
-		//Nota da P2			
+
+		//Nota da P2
   			printf (" De o valor da P2 do aluno: ");
         		scanf  ("%f", &aluno[flag].N2);
 					while (( aluno[flag].N2 > 10 ) || ( aluno[flag].N2 < 0 ))
@@ -73,10 +83,10 @@ void infoaluno(T_aluno aluno[turma])
 						printf (" Nota invalida, insira novamente : ");
 						scanf ("%f", &aluno[flag].N2);
 					}
-			
+
 			aluno[flag].media = ( (aluno[flag].N1*peso1) + (aluno[flag].N2)*peso2 )/(peso1+peso2) ;
-		
-		printf ("\n***************************************************************************************************\n");		
+
+		printf ("\n***************************************************************************************************\n");
 }
 
 void printaluno(T_aluno aluno[turma])
@@ -85,8 +95,8 @@ void printaluno(T_aluno aluno[turma])
 	for( a = 0 ; a < turma ; a++ )
 		{
 			printf (" Nome : %s", aluno[a].nome);
-			printf (" Numero USP :  %d\n", aluno[a].nro);
-			printf (" Media : %.2f\n\n", aluno[a].media);	
+			printf ("\n Numero USP :  %d\n", aluno[a].nro);
+			printf (" Media : %.2f\n\n", aluno[a].media);
 		}
 }
 
@@ -96,27 +106,27 @@ void salvando(T_aluno aluno[turma])
 		{
 //Abre o arquivo
 		FILE *pf;
-			if (( pf = fopen("teste.txt", "a" )) == NULL)	
+			if (( pf = fopen("teste.txt", "a" )) == NULL)
 				{
 					printf (" Erro na abertura do arquivo! ");
 					exit (1);
 				}
 
-//Grava informações	
+//Grava informações
 			fputs(pula, pf);
 				fputs (nome, pf);
-					fputs (aluno[i].nome, pf);	
+					fputs (aluno[i].nome, pf);
 				fputs (nro, pf);
 					fprintf (pf, "%d", aluno[i].nro);
 				fputs (N1, pf);
 					fprintf (pf, "%.2f", aluno[i].N1);
-				fputs (N2, pf);			
+				fputs (N2, pf);
 					fprintf (pf, "%.2f", aluno[i].N2);
 				fputs (media, pf);
 					fprintf (pf, "%.2f.", aluno[i].media);
 			fputs(pula, pf);
 			fclose(pf);
-			
+
 		}
 }
 
@@ -130,63 +140,65 @@ void chkaluno(T_aluno aluno[turma], T_aluno aprov[turma], T_aluno reprov[turma])
 			if (aluno[i].media >= medaprov)
 				{
 					strcpy ( aprov[a].nome, aluno[i].nome ) ;
-					aprov[a].nro = aluno[i].nro ; 
+					aprov[a].nro = aluno[i].nro ;
 					aprov[a].media = aluno[i].media ;
 					a++;
 				}
 			else
 				{
 					strcpy ( reprov[r].nome, aluno[i].nome ) ;
-					reprov[r].nro = aluno[i].nro ; 
+					reprov[r].nro = aluno[i].nro ;
 					reprov[r].media = aluno[i].media ;
 					r++;
-				}	
+				}
 		}
 }
 
 void alunoaprov(T_aluno aprov[turma])
 {
-	printf ("\n***************************************************************************************************\n");		
+	printf ("\n***************************************************************************************************\n");
 	i = a;
 	printf (" O numero de alunos aprovados sao %d\n\n", a);
 	for( a = 0 ; a < i ; a++ )
 		{
 			printf (" Nome : %s", aprov[a].nome);
-			printf (" Numero USP :  %d\n", aprov[a].nro);	
-			printf (" Media : %.2f\n\n", aprov[a].media);	
+			printf ("\n Numero USP :  %d\n", aprov[a].nro);
+			printf (" Media : %.2f\n\n", aprov[a].media);
 		}
 }
 
 void alunoreprov(T_aluno reprov[turma])
 {
-	printf ("\n***************************************************************************************************\n");	
+	printf ("\n***************************************************************************************************\n");
 	i = r;
 	printf (" O numero de alunos reprovados sao %d\n\n", r);
 	for( r = 0 ; r < i ; r++ )
 		{
 			printf (" Nome : %s", reprov[r].nome);
-			printf (" Numero USP :  %d\n", reprov[r].nro);	
-			printf (" Media : %.2f\n\n", reprov[r].media);	
+			printf ("\n Numero USP :  %d\n", reprov[r].nro);
+			printf (" Media : %.2f\n\n", reprov[r].media);
 		}
 }
 
 int main ()
-{			
+{
 	printf (" Esse programa vai pegar dados da turma, dos alunos, salvar e verificar");
-		
+
 		infoturma();
-		
+
 	T_aluno aluno[turma], aprov[turma], reprov[turma];
-		
+
 		while (flag < turma)
 			{
+				b = 0;
+				nomealuno(aluno);
 				infoaluno(aluno);
 				flag++;
 			}
-			
-	printf("\n");	
+
+	printf("\n");
 		printaluno(aluno);
-	printf("\n");	
+	printf("\n");
 		chkaluno(aluno, aprov, reprov);
 	printf("\n");
 		alunoaprov(aprov);
@@ -194,7 +206,7 @@ int main ()
 		alunoreprov(reprov);
 	printf("\n");
 		salvando(aluno);
-		
+
 	system ("PAUSE");
 	return(0);
 }
